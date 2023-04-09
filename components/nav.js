27643a1c8e0a0e737a8fbe73bc3c1191374b3322
Junya-0220 <junya010220@gmail.com>
2023-04-09 15:37:23 +0900

    追加

--- components/nav.js
+++ components/nav.js
@@ -5,7 +5,11 @@ export default function Nav() {
     <ul>
       <li>
         <Link href="/">Home</Link>
+      </li>
+      <li>
         <Link href="/about">About</Link>
+      </li>
+      <li>
         <Link href="/blog">Blog</Link>
       </li>
     </ul>
